bb51731eb29eb5520aa49fcabb063549d4d4fcc1
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-02-23 23:17:47 +0000

    2.0.0 +++ #45775
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/caddy/trunk@71433 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -1,35 +1,187 @@
 <?php
-if (!defined ('TYPO3_MODE')) {
-	die ('Access denied.');
+if( ! defined( 'TYPO3_MODE' ) )
+{
+  die( 'Access denied.' );
 }
 
-t3lib_extMgm::addStaticFile($_EXTKEY, 'static/', 'Caddy');
-t3lib_extMgm::addStaticFile($_EXTKEY, 'static/css/', '+Caddy CSS');
+
+
+  ////////////////////////////////////////////////////////////////////////////
+  //
+  // INDEX
+
+  // Set TYPO3 version
+  // Configuration by the extension manager
+  //    Localization support
+  // Enables the Include Static Templates
+  // Add pagetree icons
+  // Methods for backend workflows
+  // TCA for tt_content
+
+
+
+  ////////////////////////////////////////////////////////////////////////////
+  //
+  // Set TYPO3 version
+
+  // Set TYPO3 version as integer (sample: 4.7.7 -> 4007007)
+list( $main, $sub, $bugfix ) = explode( '.', TYPO3_version );
+$version = ( ( int ) $main ) * 1000000;
+$version = $version + ( ( int ) $sub ) * 1000;
+$version = $version + ( ( int ) $bugfix ) * 1;
+$typo3Version = $version;
+  // Set TYPO3 version as integer (sample: 4.7.7 -> 4007007)
+
+if( $typo3Version < 3000000 ) 
+{
+  $prompt = '<h1>ERROR</h1>
+    <h2>Unproper TYPO3 version</h2>
+    <ul>
+      <li>
+        TYPO3 version is smaller than 3.0.0
+      </li>
+      <li>
+        constant TYPO3_version: ' . TYPO3_version . '
+      </li>
+      <li>
+        integer $this->typo3Version: ' . ( int ) $this->typo3Version . '
+      </li>
+    </ul>
+      ';
+  die ( $prompt );
+}
+  // Set TYPO3 version
+
+
+    
+
+  ////////////////////////////////////////////////////////////////////////////
+  //
+  // Configuration by the extension manager
+
+$confArr  = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['flipit']);
+
+  // Language for labels of static templates and page tsConfig
+$beLanguage = $confArr['beLanguage'];
+switch( $beLanguage )
+{
+  case( 'German'):
+    $beLanguage = 'de';
+    break;
+  default:
+    $beLanguage = 'default';
+}
+  // Language for labels of static templates and page tsConfig
+  // Configuration by the extension manager
+
+
+
+
+  ////////////////////////////////////////////////////////////////////////////
+  //
+  // Enables the Include Static Templates
+
+  // Case $beLanguage
+switch( true ) 
+{
+  case( $beLanguage == 'de' ):
+      // German
+    t3lib_extMgm::addStaticFile($_EXTKEY, 'static/', 'Caddy');
+    t3lib_extMgm::addStaticFile($_EXTKEY, 'static/css/', '+Caddy CSS');
+//    switch( true )
+//    {
+//      case( $typo3Version < 4007000 ):
+//        t3lib_extMgm::addStaticFile($_EXTKEY,'static/typo3/4.6/', '+Flip it!: Basis fuer TYPO3 < 4.7 (einbinden!)');
+//        break;
+//      default:
+//        t3lib_extMgm::addStaticFile($_EXTKEY,'static/typo3/4.6/', '+Flip it!: Basis fuer TYPO3 < 4.7 (NICHT einbinden!)');
+//        break;
+//    }
+    break;
+  default:
+      // English
+    t3lib_extMgm::addStaticFile($_EXTKEY, 'static/', 'Caddy');
+    t3lib_extMgm::addStaticFile($_EXTKEY, 'static/css/', '+Caddy CSS');
+//    switch( true )
+//    {
+//      case( $typo3Version < 4007000 ):
+//        t3lib_extMgm::addStaticFile($_EXTKEY,'static/typo3/4.6/', '+Flip it!: Basis for TYPO3 < 4.7 (obligate!)');
+//        break;
+//      default:
+//        t3lib_extMgm::addStaticFile($_EXTKEY,'static/typo3/4.6/', '+Flip it!: Basis for TYPO3 < 4.7 (don\'t use it!)');
+//        break;
+//    }
+    break;
+}
+  // Case $beLanguage
+  // Enables the Include Static Templates
+  
+
+
+  ////////////////////////////////////////////////////////////////////////////
+  //
+  // Add pagetree icons
+
+  // Case $beLanguage
+switch( true )
+{
+  case( $beLanguage == 'de' ):
+      // German
+    $TCA['pages']['columns']['module']['config']['items'][] =
+       array( 'Caddy', 'caddy', t3lib_extMgm::extRelPath( $_EXTKEY ).'ext_icon.gif' );
+    break;
+  default:
+      // English
+    $TCA['pages']['columns']['module']['config']['items'][] =
+       array( 'Caddy', 'caddy', t3lib_extMgm::extRelPath( $_EXTKEY ).'ext_icon.gif' );
+}
+  // Case $beLanguage
+
+t3lib_SpriteManager::addTcaTypeIcon('pages', 'contains-caddy', '../typo3conf/ext/caddy/ext_icon.gif');
+  // Add pagetree icons
+
+
+
+  ///////////////////////////////////////////////////////////
+  //
+  // Methods for backend workflows
+
+//require_once(t3lib_extMgm::extPath($_EXTKEY).'lib/flexform/class.tx_caddy_flexform.php');
+//require_once(t3lib_extMgm::extPath($_EXTKEY).'lib/userfunc/class.tx_caddy_userfunc.php');
+  // Methods for backend workflows
+
+
+
+  ////////////////////////////////////////////////////////////////////////////
+  //
+  // TCA for tt_content
 
 t3lib_div::loadTCA('tt_content');
+
 $TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1'] = 'layout,select_key,pages';
 t3lib_extMgm::addPlugin(array(
-	'LLL:EXT:caddy/locallang_db.xml:tt_content.list_type_pi1',
-	$_EXTKEY . '_pi1',
-	t3lib_extMgm::extRelPath($_EXTKEY) . 'ext_icon.gif'
+  'LLL:EXT:caddy/locallang_db.xml:tt_content.list_type_pi1',
+  $_EXTKEY . '_pi1',
+  t3lib_extMgm::extRelPath($_EXTKEY) . 'ext_icon.gif'
 ),'list_type');
 
 $TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi2']  = 'layout,select_key,pages';
 $TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY.'_pi2']      ='pi_flexform';
 t3lib_extMgm::addPlugin(array(
-	'LLL:EXT:caddy/locallang_db.xml:tt_content.list_type_pi2',
-	$_EXTKEY . '_pi2',
-	t3lib_extMgm::extRelPath($_EXTKEY) . 'ext_icon.gif'
+  'LLL:EXT:caddy/locallang_db.xml:tt_content.list_type_pi2',
+  $_EXTKEY . '_pi2',
+  t3lib_extMgm::extRelPath($_EXTKEY) . 'ext_icon.gif'
 ),'list_type');
 t3lib_extMgm::addPiFlexFormValue($_EXTKEY.'_pi2', 'FILE:EXT:'.$_EXTKEY.'/pi2/flexform_ds.xml'); 
 
 $TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi3']  = 'layout,select_key,pages';
 $TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY.'_pi3']      ='pi_flexform';
 t3lib_extMgm::addPlugin(array(
-	'LLL:EXT:caddy/locallang_db.xml:tt_content.list_type_pi3',
-	$_EXTKEY . '_pi3',
-	t3lib_extMgm::extRelPath($_EXTKEY) . 'ext_icon.gif'
+  'LLL:EXT:caddy/locallang_db.xml:tt_content.list_type_pi3',
+  $_EXTKEY . '_pi3',
+  t3lib_extMgm::extRelPath($_EXTKEY) . 'ext_icon.gif'
 ),'list_type');
 t3lib_extMgm::addPiFlexFormValue($_EXTKEY.'_pi3', 'FILE:EXT:'.$_EXTKEY.'/pi3/flexform_ds.xml');
+  // TCA for tt_content
 
 ?>
