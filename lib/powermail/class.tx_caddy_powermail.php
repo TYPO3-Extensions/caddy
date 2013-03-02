66f562a94ba8671de9c869a49f18860690ee894d
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-03-02 19:39:26 +0000

    2.0.0 +++ #45954
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/caddy/trunk@72014 735d13b6-9817-0410-8766-e36946ffe9aa

--- lib/powermail/class.tx_caddy_powermail.php
+++ lib/powermail/class.tx_caddy_powermail.php
@@ -827,7 +827,6 @@ class tx_caddy_powermail
     $prompt = __METHOD__ . ' (' . __LINE__ . ')';
     t3lib_div::devlog( '[INFO/POWERMAIL] ' . $prompt, $this->extKey, 0 );
     return 'typo3conf/ext/caddy/files/pdf/default/typo3-quick-shop-rechnung_A4.pdf';
-    return 'typo3conf/ext/quick_shop/res/files/de/typo3-quick-shop-agb_A4.pdf';
   }
 
  /**
