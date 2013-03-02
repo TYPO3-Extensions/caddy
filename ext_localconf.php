7369fc09d614c451f0107426b29e5cb8b9aee76a
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-03-02 11:41:26 +0000

    2.0.0 +++ #45954
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/caddy/trunk@71929 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -13,16 +13,16 @@ t3lib_extMgm::addPItoST43( $_EXTKEY, 'pi2/class.tx_caddy_pi2.php', '_pi2', 'list
 t3lib_extMgm::addPItoST43( $_EXTKEY, 'pi3/class.tx_caddy_pi3.php', '_pi3', 'list_type', 1 );
 
 # Hook: clear powermail output if session is not filled
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['powermail']['PM_MainContentHookAfter'][]  = 'EXT:caddy/lib/class.tx_caddy_powermail_146.php:tx_caddy_powermail_146';
-
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['powermail']['PM_SubmitEmailHook'][]       = 'EXT:caddy/lib/class.tx_caddy_powermail_146.php:tx_caddy_powermail_146';
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['powermail']['PM_SubmitEmailHook2'][]      = 'EXT:caddy/lib/class.tx_caddy_powermail_146.php:tx_caddy_powermail_146';
-
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['powermail']['PM_MandatoryHookBefore'][]   = 'EXT:caddy/lib/class.tx_caddy_powermail_146.php:tx_caddy_powermail_146';
-
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['powermail']['PM_MandatoryHook'][]         = 'EXT:caddy/lib/class.tx_caddy_powermail_146.php:tx_caddy_powermail_146';
-
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['powermail']['PM_SubmitLastOne'][]         = 'EXT:caddy/lib/class.tx_caddy_powermail_146.php:tx_caddy_powermail_146';
+//$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['powermail']['PM_MainContentHookAfter'][]  = 'EXT:caddy/lib/class.tx_caddy_powermail_146.php:tx_caddy_powermail_146';
+//
+//$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['powermail']['PM_SubmitEmailHook'][]       = 'EXT:caddy/lib/class.tx_caddy_powermail_146.php:tx_caddy_powermail_146';
+//$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['powermail']['PM_SubmitEmailHook2'][]      = 'EXT:caddy/lib/class.tx_caddy_powermail_146.php:tx_caddy_powermail_146';
+//
+//$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['powermail']['PM_MandatoryHookBefore'][]   = 'EXT:caddy/lib/class.tx_caddy_powermail_146.php:tx_caddy_powermail_146';
+//
+//$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['powermail']['PM_MandatoryHook'][]         = 'EXT:caddy/lib/class.tx_caddy_powermail_146.php:tx_caddy_powermail_146';
+//
+//$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['powermail']['PM_SubmitLastOne'][]         = 'EXT:caddy/lib/class.tx_caddy_powermail_146.php:tx_caddy_powermail_146';
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tce']['formevals']['tx_caddy_evalprice'] = 'EXT:caddy/pi2/class.tx_caddy_evalprice.php';
 ?>
