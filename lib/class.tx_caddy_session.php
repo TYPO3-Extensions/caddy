02a297db20d72b6b21d8b430cc911c5a3ab880a8
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-03-13 00:09:06 +0000

    2.0.0 +++ #45954
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/caddy/trunk@72776 735d13b6-9817-0410-8766-e36946ffe9aa

--- lib/class.tx_caddy_session.php
+++ lib/class.tx_caddy_session.php
@@ -851,7 +851,7 @@ class tx_caddy_session
                       + $value;
     }
 
-var_dump( __METHOD__, __LINE__, $itemsQuantity );
+var_dump( __METHOD__, __LINE__, $this->pObj->piVars['qty'], $itemsQuantity );
 
       // RETURN : max quantity for all items isn't overrun
     if( $itemsQuantity <= $itemsQuantityMax )
