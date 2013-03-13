81ce4ff1b7d9653c2d012c53bb4d0f8aa7166d16
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-03-13 21:26:11 +0000

    2.0.0 +++ #45954
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/caddy/trunk@72820 735d13b6-9817-0410-8766-e36946ffe9aa

--- lib/class.tx_caddy_session.php
+++ lib/class.tx_caddy_session.php
@@ -775,7 +775,8 @@ var_dump( __METHOD__, __LINE__, 'quantityCheckMinMax( )' );
           // DRS
           // limit is overrun
         $product['qty'] = $product['max'];
-        $this->pObj->piVars['qty'][$product['puid']] = $product['qty'];
+//        $this->pObj->piVars['qty'][$product['puid']] = $product['qty'];
+        $this->pObj->piVars['qty'] = $product['qty'];
         $llKey          = 'caddy_ll_error_max';
         $llAlt          = 'No value for caddy_ll_error_max in ' . __METHOD__ . ' (' . __LINE__ .')';
         $llPrompt       = $this->pObj->pi_getLL( $llKey, $llAlt );
@@ -842,7 +843,8 @@ var_dump( __METHOD__, __LINE__, 'quantityCheckMinMax( )' );
         }
           // DRS
         $product['qty'] = $product['min'];
-        $this->pObj->piVars['qty'][$product['puid']] = $product['qty'];
+//        $this->pObj->piVars['qty'][$product['puid']] = $product['qty'];
+        $this->pObj->piVars['qty'] = $product['qty'];
         $llKey          = 'caddy_ll_error_min';
         $llAlt          = 'No value for caddy_ll_error_min in ' . __METHOD__ . ' (' . __LINE__ .')';
         $llPrompt       = $this->pObj->pi_getLL( $llKey, $llAlt );
