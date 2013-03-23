7a31de23fb1de515dd7773510d084cb0dfd3422b
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-03-23 15:39:33 +0000

    2.0.2 - #i0003
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/caddy/trunk@73517 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi3/class.tx_caddy_pi3.php
+++ pi3/class.tx_caddy_pi3.php
@@ -183,7 +183,7 @@ var_dump( __METHOD__, __LINE__, $this->pidCaddy, $this->products, $this->tmpl );
     $this->drs->row         = $this->cObj->data;
 
     require_once( $path2lib . 'class.tx_caddy_dynamicmarkers.php' );
-    $this->dynamicMarkers = t3lib_div::makeInstance(' tx_caddy_dynamicmarkers' );
+    $this->dynamicMarkers = t3lib_div::makeInstance( 'tx_caddy_dynamicmarkers' );
 
     require_once( 'class.tx_caddy_pi1_flexform.php' );
     $this->flexform         = t3lib_div::makeInstance( 'tx_caddy_pi1_flexform' );
