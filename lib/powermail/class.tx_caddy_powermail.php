e9693da8fe958e4ee5416a1b60e6c07343507145
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-03-27 00:38:34 +0000

    2.0.2 - Improve Calc
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/caddy/trunk@73812 735d13b6-9817-0410-8766-e36946ffe9aa

--- lib/powermail/class.tx_caddy_powermail.php
+++ lib/powermail/class.tx_caddy_powermail.php
@@ -240,7 +240,7 @@ $content = $content . $this->cObj->substituteMarkerArrayCached
 
 $content = $this->dynamicMarkers->main( $content, $this ); // Fill dynamic locallang or typoscript markers
 $content = preg_replace( '|###.*?###|i', '', $content ); // Finally clear not filled markers
-var_dump( __METHOD__, __LINE__, $this->caddy->caddy( ) );
+var_dump( __METHOD__, __LINE__, $content );
 die( );
 return $content;
     
