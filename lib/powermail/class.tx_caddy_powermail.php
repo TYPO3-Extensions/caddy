dd5e5ff3404a0391d3d81834c9cc8ab36280a3f3
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-06-28 09:52:17 +0000

    2.0.8: #i0019
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/caddy/trunk@76844 735d13b6-9817-0410-8766-e36946ffe9aa

--- lib/powermail/class.tx_caddy_powermail.php
+++ lib/powermail/class.tx_caddy_powermail.php
@@ -206,12 +206,13 @@ class tx_caddy_powermail extends tslib_pibase
     $content = preg_replace( '|###.*?###|i', '', $content ); // Finally clear not filled markers
 
     $content = html_entity_decode( $content );
+    $content = utf8_encode( $content );
 
 //$content = str_replace( '&nbsp;', ' ', $content );
 //$content = str_replace( '&uuml;', 'ü', $content );
-var_dump( __METHOD__, __LINE__, $content );
-var_dump( __METHOD__, __LINE__, utf8_encode( $content ) );
-die( __METHOD__ . '#' . __LINE__ );
+//var_dump( __METHOD__, __LINE__, $content );
+//var_dump( __METHOD__, __LINE__, utf8_encode( $content ) );
+//die( __METHOD__ . '#' . __LINE__ );
     return $content;
   }
 
