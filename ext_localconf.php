2ddfb29f8015d738ff9cbfc6101971845a390975
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-09-11 13:59:02 +0000

    2.1.0 +++ #51916
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/caddy/trunk@79692 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -9,6 +9,9 @@ $cached = false;
 t3lib_extMgm::addPItoST43( $_EXTKEY, 'pi1/class.tx_caddy_pi1.php', '_pi1', 'list_type', $cached );
 
 $cached = false;
+t3lib_extMgm::addPItoST43( $_EXTKEY, 'pi3/class.tx_caddy_pi2.php', '_pi2', 'list_type', $cached );
+
+$cached = false;
 t3lib_extMgm::addPItoST43( $_EXTKEY, 'pi3/class.tx_caddy_pi3.php', '_pi3', 'list_type', $cached );
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tce']['formevals']['tx_caddy_evalprice'] = 'EXT:caddy/pi2/class.tx_caddy_evalprice.php';
