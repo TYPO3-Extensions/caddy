e58a4e0ca033e1200bf0f43b417f0b6d5f404d23
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-09-11 14:01:07 +0000

    2.1.0 +++ #51916
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/caddy/trunk@79694 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -9,7 +9,7 @@ $cached = false;
 t3lib_extMgm::addPItoST43( $_EXTKEY, 'pi1/class.tx_caddy_pi1.php', '_pi1', 'list_type', $cached );
 
 $cached = false;
-t3lib_extMgm::addPItoST43( $_EXTKEY, 'pi3/class.tx_caddy_pi2.php', '_pi2', 'list_type', $cached );
+t3lib_extMgm::addPItoST43( $_EXTKEY, 'pi2/class.tx_caddy_pi2.php', '_pi2', 'list_type', $cached );
 
 $cached = false;
 t3lib_extMgm::addPItoST43( $_EXTKEY, 'pi3/class.tx_caddy_pi3.php', '_pi3', 'list_type', $cached );
