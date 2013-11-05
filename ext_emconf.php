69a14e405c57be32e7fec805dbb047018609d604
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-11-05 22:46:51 +0000

    3.0.0 +++ #53360
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/caddy/trunk@81225 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -38,7 +38,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'depends' => array(
 			'powermail' => '',
                         't3_tcpdf'  => '',
-			'typo3' => '4.5.0-4.7.99',
+			'typo3' => '4.5.0-6.1.99',
 		),
 		'conflicts' => array(
 		),
