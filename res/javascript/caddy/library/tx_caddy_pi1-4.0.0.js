440802f9b0669e0444a386c84d7a238638eef85d
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-11-15 23:21:01 +0000

    4.0.0 +++ #53679
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/caddy/trunk@81503 735d13b6-9817-0410-8766-e36946ffe9aa

--- res/javascript/caddy/library/tx_caddy_pi1-4.0.0.js
+++ res/javascript/caddy/library/tx_caddy_pi1-4.0.0.js
@@ -24,11 +24,11 @@ $( document ).ready( function( )
       e.preventDefault( ); // Don't execute the click
 
         // RETURN : current id isn't part of the DOM
-      if( ! $( "#c###UID###-singleview" ).length )
+      if( ! $( "#c###UID###" ).length )
       {
         if( t3caddyAlert )
         {
-          alert( "ERROR: The selector \"#c###UID###-singleview\" isn't part of the DOM!");
+          alert( "ERROR: The selector \"#c###UID###\" isn't part of the DOM!");
         }
         return;
       }
@@ -36,7 +36,7 @@ $( document ).ready( function( )
         
         // Update the content with the id #c###UID###-###VIEW###view
       var url                       = $( this ).t3caddy( 'url_autoQm', $( this ).attr( "href" ), "type=###TYPENUM###" );
-      var html_element              = "#c###UID###-singleview";
+      var html_element              = "#c###UID###";
       var html_element_wi_selector  = html_element + " > *";
       $( this ).t3caddy( 'update', html_element, url, html_element_wi_selector );
         // Update the content with the id #c###UID###-###VIEW###view
