9ac11fb79c578807393fb25007deb45ed4bf5aff
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-11-16 12:51:57 +0000

    4.0.0 +++ #53679
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/caddy/trunk@81534 735d13b6-9817-0410-8766-e36946ffe9aa

--- res/javascript/caddy/library/tx_caddy_pi1-4.0.0.js
+++ res/javascript/caddy/library/tx_caddy_pi1-4.0.0.js
@@ -17,25 +17,27 @@ var t3caddyAlert = ###T3CADDYALERT###;
   
 $( document ).ready( function( )
 {
-    // User has clicked the record browser
-  $( ".c###UID###" ).live(
+    // User has clicked the check-out-button for address-data
+  $( ".c###UID###-address-data" ).live(
     'click',
     function( e ) {
       e.preventDefault( ); // Don't execute the click
 
         // RETURN : current id isn't part of the DOM
-      if( ! $( "#c###UID###" ).length )
+      //if( ! $( "#c###UID###" ).length )
+      if( ! $( "#content" ).length )
       {
         if( t3caddyAlert )
         {
-          alert( "ERROR: The selector \"#c###UID###\" isn't part of the DOM!");
+          //alert( "ERROR: The selector \"#c###UID###\" isn't part of the DOM!");
+          alert( "ERROR: The selector \"#content\" isn't part of the DOM!");
         }
         return;
       }
         // RETURN : current id isn't part of the DOM
         
         // Update the content with the id #c###UID###-###VIEW###view
-      var url                       = $( this ).t3caddy( 'url_autoQm', $( this ).attr( "href" ), "type=###TYPENUM###" );
+      var url                       = $( this ).t3caddy( 'url_autoQm', $( this ).attr( "href" ), "type=###TYPENUM_ADDRESS_DATA###" );
       //var html_element              = "#c###UID###";
       var html_element              = "#content";
       var html_element_wi_selector  = html_element + " > *";
@@ -43,5 +45,5 @@ $( document ).ready( function( )
         // Update the content with the id #c###UID###-###VIEW###view
     }
   );
-    // User has clicked the record browser
+    // User has clicked the check-out-button for address-data
 });
