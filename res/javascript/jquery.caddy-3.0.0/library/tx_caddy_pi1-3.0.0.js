684ada540a41eaa68e7144ac9de32a88048a31f4
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-11-29 21:08:00 +0000

    3.0.1 +++ #53678
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/caddy/trunk@81844 735d13b6-9817-0410-8766-e36946ffe9aa

--- res/javascript/jquery.caddy-3.0.0/library/tx_caddy_pi1-3.0.0.js
+++ res/javascript/jquery.caddy-3.0.0/library/tx_caddy_pi1-3.0.0.js
@@ -102,13 +102,13 @@ $(document).on("click", ".c###UID###-step", function(e) {
   var html_element              = "#content";
   var html_element_wi_selector  = html_element + " > *";
   $( this ).t3caddy( 'update', html_element, url, html_element_wi_selector );
-  $(function() {
-//    alert( 3 ); 
-    c###UID###Accordion();
-//    alert( 4 ); 
-  });
+//  $(function() {
+////    alert( 3 ); 
+//    c###UID###Accordion();
+////    alert( 4 ); 
+//  });
   // Update the content with the id #c###UID###-###VIEW###view
-}); // User has clicked a tag with the cUID-step class
+}).c###UID###Accordion(); // User has clicked a tag with the cUID-step class
 /* AJAX end */
 
 /* Overlay begin */
