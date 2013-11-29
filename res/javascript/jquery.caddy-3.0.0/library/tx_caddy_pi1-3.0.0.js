4cea897703bc1c101d5c8f79e99902fb10fe354a
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-11-29 15:06:19 +0000

    3.0.1 +++ #53678
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/caddy/trunk@81802 735d13b6-9817-0410-8766-e36946ffe9aa

--- res/javascript/jquery.caddy-3.0.0/library/tx_caddy_pi1-3.0.0.js
+++ res/javascript/jquery.caddy-3.0.0/library/tx_caddy_pi1-3.0.0.js
@@ -3,21 +3,78 @@
  * Copyright (c) 2011-2013 Dirk Wildt
  * http://wildt.at.die-netzmacher.de/
  *
- * Version 4.0.0
+ * Version 3.0.1
  *
- * jquery.t3caddy-x.x.x.js is needed:
- *   http://docs.jquery.com/Plugins/t3caddy
+ * jquery.t3caddy-x.x.x.js is needed
  *
  * Dual licensed under the MIT and GPL licenses:
  *   http://www.opensource.org/licenses/mit-license.php
  *   http://www.gnu.org/licenses/gpl.html
  */
 
-var t3caddyAlert = ###T3CADDYALERT###;
-  
-$( document ).ready( function( )
+var indexOfPowermailAccordion = ###T3CADDYALERT###;
+var t3caddyAlert              = ###T3CADDYALERT###;
+
+
+/* Accordion begin */
+$(function() {
+  // The accordian panes of the caddy
+  $("#accordion").tabs( "#accordion div.pane",
+  {
+    tabs          : 'h2', 
+    effect        : 'slide', 
+    initialIndex  : 0,
+    onBeforeClick : function( event, indexAccordionDest ) {
+      // Get index of the current accordion tab
+      var indexAccordionSrce = this.getIndex();
+      // If index is undefined, accordion tab is the initial tab
+      if( indexAccordionSrce == undefined )
+      {
+        // RETURN : follow the users workflow
+        return true;
+      }
+      // Hide possible error prompts of accordion tab 2, if destination accordion tab isn't the 2. tab - the powermail form
+      if ( indexAccordionDest != 2 )
+      {
+        $( "div.error" ).hide();
+      }
+      // RETURN : follow the users workflow, if destination accordion tab is 'before' the 2. tab or is the 2. tab - the powermail form
+      if ( indexAccordionDest <= 2 )
+      {
+        return true;
+      }
+      // Are all values proper of the powermail form?
+      var success = $("#myform").validator({ inputEvent: 'blur', lang: 'de' }).data('validator').checkValidity();
+      if( success )
+      {
+        // RETURN : all values are proper
+        return true;
+      }
+      // Hide possible error prompts
+      $( "div.error" ).hide();
+      // RETURN : current tab is the tab of the powermail form
+      if ( indexAccordionDest == 2 )
+      {
+        return false;
+      }
+      // Select the second tab - the powermail form
+      this.click( 2 );
+      // Get the current tab (fieldset) of the powermail form
+      var indexTabSrce  = $("ul.css-tabs").data("tabs").getIndex();
+      var idTabSrce     = '#tabs-' + indexTabSrce + ' :input';
+      // Validate HTML input fields of the current tab (fieldset)
+      $(idTabSrce).validator({ lang : 'de' }).data('validator').checkValidity();
+      // Prompt a message in a lightbox (overflow)
+      $("#promptPowermailInvalid").click();
+      return false;
+    } // onBeforeClick ...
+  }); // $("#accordion").panes ...
+}); /* Accordion end */
+
+/* AJAX begin */
+$( function( )
 {
-    // User has clicked the check-out-button
+    // User has clicked a tag with the cUID-step class
   $( ".c###UID###-step" ).live(
     'click',
     function( e ) {
@@ -33,8 +90,7 @@ $( document ).ready( function( )
           alert( "ERROR: The selector \"#content\" isn't part of the DOM!");
         }
         return;
-      }
-        // RETURN : current id isn't part of the DOM
+      } // RETURN : current id isn't part of the DOM
         
         // Update the content with the id #c###UID###-###VIEW###view
       var url                       = $( this ).t3caddy( 'url_autoQm', $( this ).attr( "href" ), "type=###TYPENUM###" );
@@ -44,7 +100,68 @@ $( document ).ready( function( )
       $( this ).t3caddy( 'update', html_element, url, html_element_wi_selector );
         // Update the content with the id #c###UID###-###VIEW###view
     }
-  );
-    // User has clicked the check-out-button
-
+  ); // User has clicked a tag with the cUID-step class
 });
+ /* AJAX end */
+
+/* Overlay begin */
+$(function() {
+  // Workaround: Use buttons to initial overlays
+  $("button[rel]").overlay({
+    mask    : '#000', 
+    effect  : 'apple'
+  }); 
+}); /* Overlay end */
+
+/* Accordion begin */
+$(function() {
+  // Configure the tabs of the powermail form
+  $("ul.css-tabs").tabs(
+  "#myform > fieldset.powermail_fieldset",
+  {
+    initialIndex  : 0, // first tab
+    onBeforeClick : function( event, indexTabDest ) {
+      // Get index of the current tab
+      var indexTabSrce = this.getIndex();
+      // If index is undefined, tab is the initial tab
+      if( indexTabSrce == undefined )
+      {
+        // follow the workflow
+        return true;
+      }
+      // Get HTML id of the current tab
+      var idTabSrce = '#tabs-' + indexTabSrce + ' :input';
+      // Validate HTML input fields of the current tab
+      var success = $(idTabSrce).validator({ lang : 'de' }).data('validator').checkValidity();
+      // RETURN true : values of the current tab (fieldset) are proper, user can left the current tab
+      if( success )
+      {
+        return true;
+      }
+      // RETURN false : values of the current tab (fieldset) aren't proper, user can't left the current tab
+      return false;
+    }
+  });
+});  // $(function() ...
+/* Accordion begin */
+
+/* Validator begin */
+$.tools.validator.localize("de", {
+  // Isn't localised
+  //'*'		: 'Der Wert wird nicht akzeptiert',
+  ':email'  	: 'Gib eine korrekte E-Mail-Adresse ab',
+  ':number' 	: 'Gib eine Nummer an',
+  ':url' 	: 'Gib eine URL an',
+  '[max]'	: 'Maximal $1 ist erlaubt',
+  '[min]'	: 'Mindestens $1 ist n&ouml;tig',
+  '[required]'	: 'Gib einen Wert an'
+}); // $.tools.validator.localize ...
+$("#myform").validator( 
+{
+  inputEvent  : 'blur',
+  lang        : 'de'
+  //singleError : true // No effect!
+}); // $("#myform").validator ...
+/* Validator end */
+
+
