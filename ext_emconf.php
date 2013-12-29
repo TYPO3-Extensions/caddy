155b91c7a10faf7cb916b45acec2afca029405b6
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-12-29 18:54:41 +0000

    4.0.3 +++ #i0039
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/caddy/trunk@82326 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -17,7 +17,7 @@ $EM_CONF[$_EXTKEY] = array(
                           'You can install Caddy with one mouse click.',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '4.0.0',
+	'version' => '4.0.3',
 	'dependencies' => 'powermail',
 	'conflicts' => '',
 	'priority' => '',
