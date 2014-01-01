7a426a74c7b9207e6c40bed54fb4602ada185370
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-01-01 18:54:10 +0000

    4.0.3 +++ #i0040
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/caddy/trunk@82535 735d13b6-9817-0410-8766-e36946ffe9aa

--- res/javascript/jquery.caddy-4.0.0/library/tx_caddy_pi1-4.0.0.js
+++ res/javascript/jquery.caddy-4.0.0/library/tx_caddy_pi1-4.0.0.js
@@ -12,7 +12,7 @@
  *   http://www.gnu.org/licenses/gpl.html
  */
 
-var pmuidfieldemail     = ###PMUIDFIELDEMAIL###
+var pmuidfieldemail     = ###PMUIDFIELDEMAIL###;
 var t3caddyAlert        = ###T3CADDYALERT###;
 var t3caddyConsoleDebug = ###T3CADDYCONSOLEDEBUG###;
 
