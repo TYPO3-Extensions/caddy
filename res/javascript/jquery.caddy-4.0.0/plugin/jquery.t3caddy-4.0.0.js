ef03323c6cd2187d0d043b98835193cc4730a563
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-01-02 21:05:03 +0000

    4.0.3 +++ #i0040
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/caddy/trunk@82741 735d13b6-9817-0410-8766-e36946ffe9aa

--- res/javascript/jquery.caddy-4.0.0/plugin/jquery.t3caddy-4.0.0.js
+++ res/javascript/jquery.caddy-4.0.0/plugin/jquery.t3caddy-4.0.0.js
@@ -675,13 +675,7 @@
 
                             // Fade out the loading *.gif, initiate buttons again
                           accordionApi = $( settings.update.accordionApi );
-  setTimeout( function( ) {
-//    accordionIndex = currAccordionIndex;
-    accordionApi = methods.accordion( settings.accordion );
-//    fnInit( ); /* Initiate Accordion */
-//    alert( accordionIndex );
-//    accordionApi.click( accordionIndex );
-  }, 2000 );
+                          accordionApi = methods.accordion.call( settings.accordion );
                           //accordionApi = methods.accordion( settings.accordion );
                           //accordionApi.destroy( );
                           clean_up( settings.update.html_element );
