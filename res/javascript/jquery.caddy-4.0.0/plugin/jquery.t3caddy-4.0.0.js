56d9270a297d8c32eb30531bf94de4c92fa425c6
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-01-02 04:53:38 +0000

    4.0.3 +++ #i0040
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/caddy/trunk@82610 735d13b6-9817-0410-8766-e36946ffe9aa

--- res/javascript/jquery.caddy-4.0.0/plugin/jquery.t3caddy-4.0.0.js
+++ res/javascript/jquery.caddy-4.0.0/plugin/jquery.t3caddy-4.0.0.js
@@ -260,7 +260,9 @@
     };  // Move the powermail form into the caddy to the tab powermail
 
     var settings = {
-      messages: {
+      accordion : {
+      },
+      messages  : {
         errMissingTagPropertyLabel: "Tag is missing:",
         errMissingTagPropertyPrmpt: "A HTML tag with an attribute {0} is missing. AJAX can't work proper!",
         errServerErrorPrmpt:        "This is a message from the server. Maybe the server has some problems in general. " +
