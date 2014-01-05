ecbedcb08821644120c2902918d85a5f90ee0b05
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-01-05 22:28:13 +0000

    4.0.4 +++ #i0046
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/caddy/trunk@82907 735d13b6-9817-0410-8766-e36946ffe9aa

--- res/javascript/jquery.caddy-4.0.0/plugin/localisation-4.0.0.de.js
+++ res/javascript/jquery.caddy-4.0.0/plugin/localisation-4.0.0.de.js
@@ -14,7 +14,7 @@
 
 
 
-  $( this ).t3caddy.messages.update = {
+  $.t3caddy.messages.update = {
     errMissingTagPropertyLabel  : "Tag is missing:",
     errMissingTagPropertyPrmpt  : "A HTML tag with an attribute {0} is missing. AJAX can't work proper!",
     errServerErrorPrmpt         : "This is a message from the server. Maybe the server has some problems in general. " +
