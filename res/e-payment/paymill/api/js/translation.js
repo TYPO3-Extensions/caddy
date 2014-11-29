edc1c16a8e526e490ba8d90d1bce540d0b14a4fa
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-11-29 21:11:00 +0000

    6.0.3 +++ #i0063
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/caddy/trunk@87059 735d13b6-9817-0410-8766-e36946ffe9aa

--- res/e-payment/paymill/api/js/translation.js
+++ res/e-payment/paymill/api/js/translation.js
@@ -18,7 +18,7 @@ translation["de"]["error"]["invalid-elv-iban"]            = 'Bitte geben Sie ein
 translation["de"]["error"]["invalid-elv-iban-holder"]     = 'Bitte geben Sie den korrekten Kontoinhaber an.';
 translation["de"]["error"]["invalid_public_key"]          = 'Dem Formular fehlt der PAYMILL_PUBLIC_KEY. Der PAYMILL_PUBLIC_KEY wird hier eingerichtet: TYPO3 Constant Editor > Kategorie [CADDY - E-PAYMENT - PAYMILL]. Berichte diesen Fehler bitte Deinem TYPO3-Integrator.';
 translation["de"]["error"]["payment_not_testdata"]        = 'Bei den Daten handelt es sich leider nicht um die definierten Testdaten. Bitte geben Sie die richtigen Testdaten an oder wählen Sie eine andere Zahlungsmethode.';
-translation["de"]["error"]["unknown_error"]               = 'Unbekannter Fehler: Möglicherweise fehlt im Formular der Paymill-Public-API-Key.';
+translation["de"]["error"]["unknown_error"]               = 'Unbekannter Fehler: Möglicherweise sind Ihre Kontodaten falsch. Oder es fehlt im Formular der Paymill-Public-API-Key.';
 translation["de"]["error"]["unknown_payment-method"]      = 'System-Fehler: Die gewählte Zahlungsmethode ist nicht definiert. ';
 
 //English
@@ -39,5 +39,5 @@ translation["en"]["error"]["invalid-elv-iban"]            = 'Please enter a vali
 translation["en"]["error"]["invalid-elv-iban-holder"]     = 'Please enter the proper name of the account holder.';
 translation["en"]["error"]["invalid_public_key"]          = 'The form is without the PAYMILL_PUBLIC_KEY. You can configure the PAYMILL_PUBLIC_KEY at the TYPO3 Constant Editor > Kategorie [CADDY - E-PAYMENT - PAYMILL]. Please report this bug to your TYPO3 integrator.';
 translation["en"]["error"]["payment_not_testdata"]        = 'The given data aren\'t the determined test data. Please enter the proper data or choose a different payment method.';
-translation["en"]["error"]["unknown_error"]               = 'Undefined error: Maybe the Paymill public API key is missing in the form.';
+translation["en"]["error"]["unknown_error"]               = 'Undefined error: Maybe your account data aren\'t proper. Maybe the Paymill public API key is missing in the form.';
 translation["en"]["error"]["unknown_payment-method"]      = 'Fatal error: The selected payment method isn\'t defined.';
