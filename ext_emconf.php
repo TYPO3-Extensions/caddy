bbbfe18647d3ca728f7328e02672fd38283f5f30
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-04-18 19:59:57 +0000

    6.0.14 +++ #i0082
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/caddy/trunk@88855 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = array(
   ,
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '6.0.13',
+	'version' => '6.0.14',
 	'dependencies' => 'powermail',
 	'conflicts' => '',
 	'priority' => '',
